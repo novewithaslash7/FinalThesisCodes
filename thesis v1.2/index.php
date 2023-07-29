e76b20d5868c8fc7d228f3d8753d755ffb34c330
NOVE JHON PACHEJO <novejhon.pachejo@wvsu.edu.ph>
2023-07-29 17:16:33 +0800

    fix

--- thesis v1.2/index.php
+++ thesis v1.2/index.php
@@ -9,24 +9,28 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="flex justify-center items-center h-[100vh] bg-gray-400">
+    <div class="flex justify-center items-center  h-[100vh] bg-[url(../bg1.JPG)] bg-cover">
         <div class="p-5
             w-1/3
-            border
+            border-0
             rounded-xl
-            bg-white
+            bg-[#FF6D60]/70
             ">
-            <form action="" class="flex flex-col gap-5">
+            <form action="" class="flex flex-col justify-center items-center gap-5">
                 <!-- if naka php php code daa -->
                 <?php
                 
                 ?>
-                <input class="p-3 border rounded-xl border-solid" type="text" name="student_id" placeholder="Student ID">
-                <input class="p-3 border rounded-xl border-solid" type="password" name="password" placeholder="Password">
-                <button class="bg-gray-100 p-3 border rounded-xl hover:bg-gray-200 ">Login<button>
+                <h2>Welcome to ICT Assessment Test</h2>
+                <input required class="p-3 w-full border rounded-xl border-solid" type="text" name="student_id" placeholder="Student ID">
+                <input required class="p-3 w-full border rounded-xl border-solid" type="password" name="password" placeholder="Password">
+                <input class="bg-gray-100 p-3 w-full     border rounded-xl hover:bg-[#F3E99F] " type="submit" value="SIGN IN">
+                <a class="hover:underline" href="./register.php">Don't Have an Account? SIGN UP</a>
             </form>
         </div>
+        
     </div>
+    
     
     
 </body>
