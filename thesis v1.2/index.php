89a1670062010e8c0e4d133dfac5c609ce969d13
NOVE JHON PACHEJO <novejhon.pachejo@wvsu.edu.ph>
2023-07-29 11:35:39 +0800

    update login front

---
+++ thesis v1.2/index.php
@@ -0,0 +1,33 @@
+<!-- kun naka doc type frontend code -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assesment Test</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-[100vh]">
+    <!-- 100% width height -->
+    <div class="flex justify-center items-center h-full bg-gray-400">
+        <!-- flex to center with 20px pad -->
+        <div class="p-5
+            w-1/3
+            border
+            rounded-xl
+            bg-white
+            ">
+            <form action="" class="flex flex-col gap-5">
+                <!-- if naka php php code daa -->
+                <?php
+                
+                ?>
+                <input class="p-3 border rounded-xl border-solid" type="text" name="student_id" placeholder="Student ID">
+                <input class="p-3 border rounded-xl border-solid" type="password" name="password" placeholder="Password">
+                <button class="bg-gray-100 p-3 border rounded-xl ">Login<button>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
