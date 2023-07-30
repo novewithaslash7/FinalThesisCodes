a6c166b931e11da7289e5889c8a5887d0ce4f324
jpa012401 <johnpaul.damedigital@gmail.com>
2023-07-30 11:14:17 +0800

    scores ui

--- thesis v1.2/admin/scores.php
+++ thesis v1.2/admin/scores.php
@@ -8,7 +8,7 @@
     <!-- copy this in each html codes -->
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
-        table, th, td{
+        table, th, td, tr{
             border: 1px solid black;
             border-collapse: collapse;    
         }
@@ -28,6 +28,63 @@
             </li>
         </div>
     </div>
+    <div class="flex flex-col justify-center gap-5 p-5" >
+            <!-- filter form -->
+            <div>
+                <form class="flex gap-3">
+                    <!-- year -->
+                    <select class="p-3 bg-white border rounded-xl border-solid" name="year" placeholder="Year">
+                        <option selected hidden value="">Year</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                    <!-- section -->
+                    <select class="p-3 bg-white border rounded-xl border-solid" name="year" placeholder="Section">
+                        <option selected hidden value="">Section</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                    </select>
+
+                    <!-- curriculumn -->
+                    <select class="p-3 bg-white border rounded-xl border-solid" name="year" placeholder="Year">
+                        <option selected hidden value="">Curriculum</option>
+                        <option value="BS Information Technology">BS Information Technology</option>
+                        <option value="BS Information Systems">BS Information Systems</option>
+
+                    </select>
+                    <!-- ACTION -->
+                    <input class="bg-gray-100 p-3 border rounded-xl hover:bg-gray-300 " type="submit" value="Filter">
+                </form>
+
+            </div>
+            <!-- table -->
+            <div>
+                <table class="table-fixed border border-1 border-solid w-full">
+                    <tr>
+                        <th>Student ID</th>
+                        <th>Name</th>
+                        <th>Year</th>
+                        <th>Section</th>
+                        <th>Curriculum</th>
+                        <th>Score</th>                        
+                        <th>Clasification</th>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
 
     
     
